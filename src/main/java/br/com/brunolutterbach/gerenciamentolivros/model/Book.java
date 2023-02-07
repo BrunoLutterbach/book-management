5d5b36d4516bde254052e05d9498bd49a8bede1e
Bruno <brunolutterbach13@gmail.com>
2023-02-07 19:03:40 -0300

    Adicionado classe modelo Book

---
+++ src/main/java/br/com/brunolutterbach/gerenciamentolivros/model/Book.java
@@ -0,0 +1,26 @@
+package br.com.brunolutterbach.gerenciamentolivros.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private String description;
+    private String coverImage;
+    private String genre;
+    private LocalDateTime postDate = LocalDateTime.now();
+
+
+}
