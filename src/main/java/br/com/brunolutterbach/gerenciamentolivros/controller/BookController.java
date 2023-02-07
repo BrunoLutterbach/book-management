ba4befede9e52099025347f3b8fd6c5ec978752b
Bruno <brunolutterbach13@gmail.com>
2023-02-07 19:34:14 -0300

    Adicionado o método get para retornar todos os livros

--- src/main/java/br/com/brunolutterbach/gerenciamentolivros/controller/BookController.java
+++ src/main/java/br/com/brunolutterbach/gerenciamentolivros/controller/BookController.java
@@ -1,13 +1,19 @@
 package br.com.brunolutterbach.gerenciamentolivros.controller;
 
+import br.com.brunolutterbach.gerenciamentolivros.dto.BookCreationData;
 import br.com.brunolutterbach.gerenciamentolivros.dto.BookResponse;
+import br.com.brunolutterbach.gerenciamentolivros.repository.BookRepository;
 import br.com.brunolutterbach.gerenciamentolivros.service.BookService;
 import lombok.AllArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.transaction.Transactional;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/books")
@@ -15,10 +21,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookController {
 
     final BookService bookService;
+    private final BookRepository bookRepository;
 
     @GetMapping("/{id}")
     public ResponseEntity<BookResponse> getBookDetails(@PathVariable Long id) {
-        BookResponse bookResponse = bookService.getBookDetails(id);
+        var bookResponse = bookService.getBookDetails(id);
+        return ResponseEntity.ok(bookResponse);
+    }
+
+    @GetMapping()
+    public ResponseEntity<Page<BookResponse>> listAllBooks(Pageable pageable) {
+        var bookResponse = bookService.listAllBooks(pageable);
+        return ResponseEntity.ok(bookResponse);
+    }
+
+    @PostMapping()
+    @Transactional
+    public ResponseEntity<BookResponse> createBook(@RequestBody @Valid BookCreationData bookCreationData) {
+        var bookResponse = bookService.createBook(bookCreationData);
         return ResponseEntity.ok(bookResponse);
     }
 
