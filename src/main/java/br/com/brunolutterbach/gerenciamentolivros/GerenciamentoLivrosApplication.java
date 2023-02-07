d64a3905db9d7eaeb7f17943055912cf68f0253b
Bruno <brunolutterbach13@gmail.com>
2023-02-07 19:05:46 -0300

    First Commit

---
+++ src/main/java/br/com/brunolutterbach/gerenciamentolivros/GerenciamentoLivrosApplication.java
@@ -0,0 +1,13 @@
+package br.com.brunolutterbach.gerenciamentolivros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoLivrosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GerenciamentoLivrosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/brunolutterbach/gerenciamentolivros/GerenciamentoLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brunolutterbach.gerenciamentolivros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoLivrosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
