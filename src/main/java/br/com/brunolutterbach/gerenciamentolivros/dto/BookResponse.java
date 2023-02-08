2f83a2ccfa0ddaa9d6a23d97f8915952d01ac5d7
Bruno <brunolutterbach13@gmail.com>
2023-02-08 19:28:38 -0300

    Removido import sem utilização

--- src/main/java/br/com/brunolutterbach/gerenciamentolivros/dto/BookResponse.java
+++ src/main/java/br/com/brunolutterbach/gerenciamentolivros/dto/BookResponse.java
@@ -2,6 +2,8 @@ package br.com.brunolutterbach.gerenciamentolivros.dto;
 
 import br.com.brunolutterbach.gerenciamentolivros.model.Book;
 
+import java.util.Optional;
+
 public record BookResponse(Long id, String title, String author, String description, String coverImage, String genre, String postDate) {
     public BookResponse(Book book) {
         this(book.getId(), book.getTitle(), book.getAuthor(), book.getDescription(), book.getCoverImage(), book.getGenre(), book.getPostDate().toString());
