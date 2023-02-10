4b467a4e2858489c42dca51c0a3bd3e03a7f179b
Bruno <brunolutterbach13@gmail.com>
2023-02-10 20:47:09 -0300

    Remoção de comentarios

--- src/main/java/br/com/brunolutterbach/gerenciamentolivros/service/BookService.java
+++ src/main/java/br/com/brunolutterbach/gerenciamentolivros/service/BookService.java
@@ -1,8 +1,8 @@
 package br.com.brunolutterbach.gerenciamentolivros.service;
 
-import br.com.brunolutterbach.gerenciamentolivros.dto.BookCreationData;
-import br.com.brunolutterbach.gerenciamentolivros.dto.BookResponse;
-import br.com.brunolutterbach.gerenciamentolivros.dto.BookUpdateData;
+import br.com.brunolutterbach.gerenciamentolivros.dto.book.BookCreationData;
+import br.com.brunolutterbach.gerenciamentolivros.dto.book.BookResponse;
+import br.com.brunolutterbach.gerenciamentolivros.dto.book.BookUpdateData;
 import br.com.brunolutterbach.gerenciamentolivros.model.Book;
 import br.com.brunolutterbach.gerenciamentolivros.repository.BookRepository;
 import lombok.AllArgsConstructor;
@@ -16,42 +16,21 @@ public class BookService {
 
     final BookRepository repository;
 
-    /**
-     * Localiza um livro pelo id
-     * @param id
-     * @return BookResponse
-     */
     public BookResponse getBookDetails(Long id) {
         var book = repository.getReferenceById(id);
         return new BookResponse(book);
     }
 
-    /**
-      * Localiza todos os livros
-     * @param pageable
-     * @return Page<BookResponse>
-     */
     public Page<BookResponse> listAllBooks(Pageable pageable) {
         return repository.findAll(pageable).map(BookResponse::new);
     }
 
-    /**
-     * Cria um novo livro
-     * @param bookCreationData
-     * @return BookResponse
-     */
     public BookResponse createBook(BookCreationData bookCreationData) {
         var book = new Book(bookCreationData);
         repository.save(book);
         return new BookResponse(book);
     }
 
-    /**
-     * Atualiza um livro
-     * @param bookUpdateData
-     * @param id
-     * @return BookResponse
-     */
     public BookResponse updateBook(BookUpdateData bookUpdateData, Long id) {
         var book = repository.getReferenceById(id);
         book.update(bookUpdateData);
@@ -60,11 +39,6 @@ public class BookService {
 
     }
 
-    /**
-     * Deleta um livro
-     * @param id
-     * @return void
-     */
     public void deleteBook(Long id) {
         repository.deleteById(id);
     }
