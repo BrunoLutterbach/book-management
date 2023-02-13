ad73cba3c4cad509a4656644b0af80734214e53b
Bruno <brunolutterbach13@gmail.com>
2023-02-13 18:45:28 -0300

    Adicionado construtor e método para atualizar Review.java

--- src/main/java/br/com/brunolutterbach/gerenciamentolivros/model/Review.java
+++ src/main/java/br/com/brunolutterbach/gerenciamentolivros/model/Review.java
@@ -1,5 +1,7 @@
 package br.com.brunolutterbach.gerenciamentolivros.model;
 
+import br.com.brunolutterbach.gerenciamentolivros.dto.review.ReviewCreationData;
+import br.com.brunolutterbach.gerenciamentolivros.dto.review.ReviewUpdateData;
 import lombok.Data;
 
 import javax.persistence.*;
@@ -15,8 +17,26 @@ public class Review {
     private String comment;
     private double rating;
     private LocalDateTime reviewDate = LocalDateTime.now();
-    private LocalDateTime updateDate;
+    private LocalDateTime updateDate = LocalDateTime.now();
     @ManyToOne
     private Book book;
 
+    public Review() {
+    }
+
+    public Review(ReviewCreationData reviewCreationData, Book book) {
+        this.comment = reviewCreationData.comment();
+        this.rating = reviewCreationData.rating();
+        this.book = book;
+    }
+
+    public void update(ReviewUpdateData updateData) {
+        if (updateData.comment() != null) {
+            this.comment = updateData.comment();
+        }
+        if (updateData.rating() > 0) {
+            this.rating = updateData.rating();
+        }
+        this.updateDate = LocalDateTime.now();
+    }
 }
