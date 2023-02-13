43d7e6224d5180d3833d9a87cf833a88e036b059
Bruno <brunolutterbach13@gmail.com>
2023-02-13 18:54:37 -0300

    Removido o atributo UpdateDate do construtor.

--- src/main/java/br/com/brunolutterbach/gerenciamentolivros/dto/review/ReviewResponse.java
+++ src/main/java/br/com/brunolutterbach/gerenciamentolivros/dto/review/ReviewResponse.java
@@ -3,11 +3,11 @@ package br.com.brunolutterbach.gerenciamentolivros.dto.review;
 import br.com.brunolutterbach.gerenciamentolivros.dto.book.BookSummaryDTO;
 import br.com.brunolutterbach.gerenciamentolivros.model.Review;
 
-public record ReviewResponse(String id, String comment, double rating, String reviewDate, String updateDate,
+public record ReviewResponse(String id, String comment, double rating, String reviewDate,
                              BookSummaryDTO bookSummaryDTO) {
 
     public ReviewResponse(Review review) {
         this(review.getId().toString(), review.getComment(), review.getRating(), review.getReviewDate().toString(),
-                review.getUpdateDate().toString(), new BookSummaryDTO(review.getBook()));
+                new BookSummaryDTO(review.getBook()));
     }
 }
