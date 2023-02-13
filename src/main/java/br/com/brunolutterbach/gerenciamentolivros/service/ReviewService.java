a984081867f2ad52354615dd6ed50bcc4951340f
Bruno <brunolutterbach13@gmail.com>
2023-02-13 18:53:58 -0300

    Adicionado método para criar, atualizar e deletar um Review

--- src/main/java/br/com/brunolutterbach/gerenciamentolivros/service/ReviewService.java
+++ src/main/java/br/com/brunolutterbach/gerenciamentolivros/service/ReviewService.java
@@ -1,6 +1,10 @@
 package br.com.brunolutterbach.gerenciamentolivros.service;
 
+import br.com.brunolutterbach.gerenciamentolivros.dto.review.ReviewCreationData;
 import br.com.brunolutterbach.gerenciamentolivros.dto.review.ReviewResponse;
+import br.com.brunolutterbach.gerenciamentolivros.dto.review.ReviewUpdateData;
+import br.com.brunolutterbach.gerenciamentolivros.model.Review;
+import br.com.brunolutterbach.gerenciamentolivros.repository.BookRepository;
 import br.com.brunolutterbach.gerenciamentolivros.repository.ReviewRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -12,6 +16,7 @@ import org.springframework.stereotype.Service;
 public class ReviewService {
 
     final ReviewRepository repository;
+    final BookRepository bookRepository;
 
     public ReviewResponse getReviewDetails(Long id) {
         var review = repository.getReferenceById(id);
@@ -21,5 +26,23 @@ public class ReviewService {
     public Page<ReviewResponse> listAllReviews(Pageable pageable) {
         return repository.findAll(pageable).map(ReviewResponse::new);
 
+    }
+
+    public ReviewResponse createReview(ReviewCreationData reviewCreationData, Long id) {
+        var book = bookRepository.getReferenceById(id);
+        var review = new Review(reviewCreationData, book);
+        repository.save(review);
+        return new ReviewResponse(review);
+    }
+
+    public ReviewResponse updateReview(ReviewUpdateData updateData, Long id) {
+        var review = repository.getReferenceById(id);
+        review.update(updateData);
+        repository.save(review);
+        return new ReviewResponse(review);
+    }
+
+    public void deleteReview(Long id) {
+        repository.deleteById(id);
     }
 }
