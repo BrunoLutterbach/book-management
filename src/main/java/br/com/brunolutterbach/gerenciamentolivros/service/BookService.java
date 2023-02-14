b62296973d2f8b9e8f52af2b272144457f427afe
Bruno <brunolutterbach13@gmail.com>
2023-02-14 20:35:58 -0300

    Adicionado envio de e-mail quando um livro com o genero escolhido pelo usuário for cadastrado.

--- src/main/java/br/com/brunolutterbach/gerenciamentolivros/service/BookService.java
+++ src/main/java/br/com/brunolutterbach/gerenciamentolivros/service/BookService.java
@@ -5,41 +5,52 @@ import br.com.brunolutterbach.gerenciamentolivros.dto.book.BookResponse;
 import br.com.brunolutterbach.gerenciamentolivros.dto.book.BookUpdateData;
 import br.com.brunolutterbach.gerenciamentolivros.model.Book;
 import br.com.brunolutterbach.gerenciamentolivros.repository.BookRepository;
+import br.com.brunolutterbach.gerenciamentolivros.repository.UserPreferenceRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @AllArgsConstructor
 public class BookService {
 
-    final BookRepository repository;
+    final BookRepository bookRepository;
+    final UserPreferenceRepository userPreferenceRepository;
+    final EmailSenderService emailSenderService;
 
     public BookResponse getBookDetails(Long id) {
-        var book = repository.getReferenceById(id);
+        var book = bookRepository.getReferenceById(id);
         return new BookResponse(book);
     }
 
     public Page<BookResponse> listAllBooks(Pageable pageable) {
-        return repository.findAll(pageable).map(BookResponse::new);
+        return bookRepository.findAll(pageable).map(BookResponse::new);
     }
 
     public BookResponse createBook(BookCreationData bookCreationData) {
         var book = new Book(bookCreationData);
-        repository.save(book);
+        bookRepository.save(book);
+        List<String> emails = userPreferenceRepository.findAllEmailsWithGenre(book.getGenre());
+        for (String email : emails) {
+            String name = userPreferenceRepository.getNameByEmail(email);
+            emailSenderService.sendEmail(email, "[" + book.getGenre().toUpperCase() + "]" + " Novo livro cadastrado",
+                    "Olá, " + name + "! Um novo livro foi cadastrado com o gênero " + book.getGenre() + "!");
+        }
         return new BookResponse(book);
     }
 
     public BookResponse updateBook(BookUpdateData bookUpdateData, Long id) {
-        var book = repository.getReferenceById(id);
+        var book = bookRepository.getReferenceById(id);
         book.update(bookUpdateData);
-        repository.save(book);
+        bookRepository.save(book);
         return new BookResponse(book);
 
     }
 
     public void deleteBook(Long id) {
-        repository.deleteById(id);
+        bookRepository.deleteById(id);
     }
 }
